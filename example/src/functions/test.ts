a9f61175b869d41af915af5b638019f24fa83ab4
Patrick Ruhkopf <patrick@ruhkopf.me>
2024-01-03 12:30:42 +0100

    chore: add options and make request body parsing work

--- example/src/functions/test.ts
+++ example/src/functions/test.ts
@@ -1,77 +1,40 @@
 import * as fs from 'fs'
 import * as yaml from 'js-yaml'
-import { app, HttpRequest, HttpResponseInit, InvocationContext, PostInvocationContext, PreInvocationContext } from "@azure/functions";
-import { AjvOpenApiValidator } from '@restfulhead/azure-functions-nodejs-openapi-validator'
+import { app, HttpResponseInit, InvocationContext } from '@azure/functions'
+import { setupValidation, ParsedRequestBodyHttpRequest } from '@restfulhead/azure-functions-nodejs-openapi-validator'
 
+/***
+ * Load the OpenAPI spec from a file and setup the validator
+ */
 const openApiContent = fs.readFileSync(`${__dirname}/../../../../test/fixtures/openapi.yaml`, 'utf8')
-const validator = new AjvOpenApiValidator(yaml.load(openApiContent) as any)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const openApiSpec = yaml.load(openApiContent) as any
 
-const createJsonResponse = (body: any, status: number = 200, headers?: HeadersInit): HttpResponseInit => {
-    return { body: body === undefined || body === null ? undefined : JSON.stringify(body), status: 400, headers: headers ? headers : { 'Content-Type': 'application/json' } }
+setupValidation(openApiSpec)
+
+/**
+ *  This has nothing to do with the validator, just a workaround for https://github.com/Azure/azure-functions-host/issues/9238 and
+ *  https://github.com/Azure/azure-functions-host/issues/8222
+ */
+app.hook.postInvocation(async () => {
+  // Add slight delay to ensure logs show up before the invocation finishes
+  await new Promise((resolve) => setTimeout(resolve, 50))
+})
+
+// eslint-disable-next-line require-await
+export async function putUser(
+  request: ParsedRequestBodyHttpRequest<{ name: string }>,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`)
+
+  const name = request.parsedBody.name
+
+  return { body: `Hello, ${name}!` }
 }
 
-app.hook.preInvocation((preContext: PreInvocationContext) => {
-    const originalHandler = preContext.functionHandler 
-    const path = '/' + preContext.invocationContext.options.trigger.route
-
-    preContext.functionHandler = (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> => {
-        const method = request.method
-        const requestBody = request.body
-        preContext.hookData.requestMethod = method
-
-        context.log(`Validating query parameters '${path}', '${method}'`);
-        const reqParamsValResult = validator.validateQueryParams(path, method, request.query)
-        if (reqParamsValResult) {
-            preContext.hookData.requestQueryParameterValidationError = true
-            return Promise.resolve(createJsonResponse(reqParamsValResult, 400))
-        }
-        
-        context.log(`Validating request body for '${path}', '${method}'`);
-        const reqBodyValResult = validator.validateRequestBody(path, method,  requestBody, true)
-        if (reqBodyValResult) {
-            preContext.hookData.requestBodyValidationError = true
-            return Promise.resolve(createJsonResponse(reqBodyValResult, 400))
-        }
-
-        return originalHandler(request, context)
-    }
-})
-
-
-app.hook.postInvocation((postContext: PostInvocationContext) => {
-    const path = '/' + postContext.invocationContext.options.trigger.route
-    const method = postContext.hookData.requestMethod
-
-    if (postContext.result && !postContext.hookData.requestBodyValidationError && !postContext.hookData.requestQueryParameterValidationError) {
-        // TODO validate response body
-        const respBodyValResult = validator.validateResponseBody(path, method,  (postContext.result as any)?.status, true)
-        if (respBodyValResult) {
-            postContext.result = { body: respBodyValResult, status: 400 }
-        }
-    }
-})
-
-
-export async function postHelloWorld(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    const name = await request.text();
-
-    return { body: `Hello, ${name}!` };
-};
-
-
-
-app.hook.postInvocation(async () => {
-    // Add slight delay to ensure logs show up before the invocation finishes
-    // See these issues for more info:
-    // https://github.com/Azure/azure-functions-host/issues/9238
-    // https://github.com/Azure/azure-functions-host/issues/8222
-    await new Promise((resolve) => setTimeout(resolve, 50));
-});
-
 app.put('post-users-uid', {
-    route: 'users/{uid}',
-    authLevel: 'anonymous',
-    handler: postHelloWorld
-});
+  route: 'users/{uid}',
+  authLevel: 'anonymous',
+  handler: putUser,
+})
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,153 @@
-export function setup(spec: string | object) {
+import { app, HttpRequest, HttpResponseInit, InvocationContext, PreInvocationContext } from '@azure/functions'
+import { AjvOpenApiValidator } from './ajv-openapi-validator'
+import { OpenAPIV3 } from 'openapi-types'
+import { DEFAULT_AJV_SETTINGS } from './ajv-opts'
+import { DEFAULT_VALIDATOR_OPTS } from './openapi-validator'
+import { createJsonResponse, logMessage } from './helper'
+
+export interface ParsedRequestBodyHttpRequest<T> extends HttpRequest {
+  parsedBody: T | undefined
+}
+
+export interface ValidationMode {
+  /** whether to return an error response in case of a validation error instead of the actual function result */
+  returnErrorResponse: boolean
+  /** whether to log the validation error */
+  logLevel: 'debug' | 'info' | 'warn' | 'error' | 'off'
+  strict: boolean
+}
+
+export interface ValidatorHookOptions {
+  /** whether to validate query parameters and return an error response or log any errors */
+  queryParameterValidationMode: ValidationMode | false
+
+  /** whether to validate the request body and return an error response or log any errors */
+  requestBodyValidationMode: ValidationMode | false
+
+  /** whether to validate the response body and return an error response or log any errors */
+  responseBodyValidationMode: ValidationMode | false
+}
+
+/**
+ * The default hook options prevent further execution if query parameter or request body validation fails and returns an error message
+ * instead. Response body validation errors are instead logged as a warning, but still return the orginal function result.
+ */
+export const DEFAULT_HOOK_OPTIONS: ValidatorHookOptions = {
+  queryParameterValidationMode: {
+    returnErrorResponse: true,
+    logLevel: 'info',
+    strict: true,
+  },
+
+  requestBodyValidationMode: {
+    returnErrorResponse: true,
+    logLevel: 'info',
+    strict: true,
+  },
+
+  responseBodyValidationMode: {
+    returnErrorResponse: false,
+    logLevel: 'warn',
+    strict: true,
+  },
+}
+
+export function setupValidation(
+  spec: OpenAPIV3.Document,
+  opts = { hook: DEFAULT_HOOK_OPTIONS, validator: DEFAULT_VALIDATOR_OPTS, ajv: DEFAULT_AJV_SETTINGS }
+) {
   console.log('Hello World' + spec)
+
+  const validator = new AjvOpenApiValidator(spec, opts?.validator, opts?.ajv)
+
+  const requiredHookOpts = opts.hook ? { ...DEFAULT_HOOK_OPTIONS, ...opts.hook } : DEFAULT_HOOK_OPTIONS
+
+  app.hook.preInvocation((preContext: PreInvocationContext) => {
+    const originalHandler = preContext.functionHandler
+    const path = '/' + preContext.invocationContext.options.trigger.route
+
+    preContext.functionHandler = async (
+      request: ParsedRequestBodyHttpRequest<unknown>,
+      context: InvocationContext
+    ): Promise<HttpResponseInit> => {
+      const method = request.method
+
+      if (requiredHookOpts.queryParameterValidationMode) {
+        context.debug(`Validating query parameters '${path}', '${method}'`)
+        const reqParamsValResult = validator.validateQueryParams(
+          path,
+          method,
+          request.query,
+          requiredHookOpts.queryParameterValidationMode.strict
+        )
+        if (reqParamsValResult) {
+          preContext.hookData.requestQueryParameterValidationError = true
+
+          if (requiredHookOpts.queryParameterValidationMode.logLevel !== 'off') {
+            logMessage(
+              `Query param validation error: ${JSON.stringify(reqParamsValResult)}`,
+              requiredHookOpts.queryParameterValidationMode.logLevel,
+              context
+            )
+          }
+
+          if (requiredHookOpts.queryParameterValidationMode.returnErrorResponse) {
+            return Promise.resolve(createJsonResponse(reqParamsValResult, 400))
+          }
+        }
+      }
+
+      if (requiredHookOpts.requestBodyValidationMode) {
+        context.info(`Validating request body for '${path}', '${method}'`)
+        request.parsedBody = request.body ? await request.json() : undefined
+
+        const reqBodyValResult = validator.validateRequestBody(
+          path,
+          method,
+          request.parsedBody,
+          requiredHookOpts.requestBodyValidationMode.strict
+        )
+        if (reqBodyValResult) {
+          preContext.hookData.requestBodyValidationError = true
+
+          if (requiredHookOpts.requestBodyValidationMode.logLevel !== 'off') {
+            logMessage(
+              `Request body validation error: ${JSON.stringify(reqBodyValResult)}`,
+              requiredHookOpts.requestBodyValidationMode.logLevel,
+              context
+            )
+          }
+
+          if (requiredHookOpts.requestBodyValidationMode.returnErrorResponse) {
+            return Promise.resolve(createJsonResponse(reqBodyValResult, 400))
+          }
+        }
+      }
+
+      const response = await originalHandler(request, context)
+
+      if (requiredHookOpts.responseBodyValidationMode) {
+        context.debug(`Validating response body for '${path}', '${method}', '${response.status}'`)
+        const respBodyValResult = validator.validateResponseBody(
+          path,
+          method,
+          response.status,
+          requiredHookOpts.responseBodyValidationMode.strict
+        )
+        if (requiredHookOpts.responseBodyValidationMode.logLevel !== 'off') {
+          logMessage(
+            `Request body validation error: ${JSON.stringify(respBodyValResult)}`,
+            requiredHookOpts.responseBodyValidationMode.logLevel,
+            context
+          )
+        }
+
+        if (requiredHookOpts.responseBodyValidationMode.returnErrorResponse) {
+          return Promise.resolve(createJsonResponse(respBodyValResult, 500))
+        }
+      }
+
+      return response
+    }
+  })
 }
